7ab21a2a16e17e22a9dbab5216572611ce770b12
Patrick Mutter <develop@mperia.de>
2019-01-20 18:00:49 +0100

    Update example.

--- examples/example-jump/views/Home.vue
+++ examples/example-jump/views/Home.vue
@@ -3,7 +3,7 @@
     <div id="top" class="block"/>
     <anchor-router-link :to="{name: 'home', hash: '#bottom'}">To Bottom</anchor-router-link>
 
-    <h1>Demo - Route to anchor point with no animation</h1>
+    <h1>Demo - Route to anchor point with jump animation</h1>
     <h2>Instructions:</h2>
     <ul>
       <li>Make sure no #anchor-point is appended in the URL</li>
